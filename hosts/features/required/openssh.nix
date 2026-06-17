fa7e3680ac802c067b87de5603fe84655e48019f
Samuel Silva <samuel@ossystems.com.br>
2026-06-17 15:48:51 -0300

    monster: Fix auto-upgrade failing to fetch private ossystems-tools

--- hosts/features/required/openssh.nix
+++ hosts/features/required/openssh.nix
@@ -19,6 +19,9 @@
     }];
   };
 
+  programs.ssh.knownHosts."github.com".publicKey =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl";
+
   # Passwordless sudo when SSH'ing with keys
   security.pam.sshAgentAuth.enable = true;
 }
