8a1808c72dbc6c5e60c32bec530ef8b6412451c9
Samuel Silva <samuel@ossystems.com.br>
2026-06-24 17:08:00 -0300

    ci: switch formatter from nixpkgs-fmt to nixfmt (RFC style)

--- nix/checks/lint.nix
+++ nix/checks/lint.nix
@@ -1,9 +1,16 @@
 { pkgs, flake, ... }:
 
 pkgs.runCommand "lint-code"
-{ nativeBuildInputs = with pkgs; [ nixpkgs-fmt deadnix statix ]; } ''
-  deadnix --fail ${flake}
-  #statix check ${flake} # https://github.com/nerdypepper/statix/issues/75
-  nixpkgs-fmt --check ${flake}
-  touch $out
-''
+  {
+    nativeBuildInputs = with pkgs; [
+      nixfmt
+      deadnix
+      statix
+    ];
+  }
+  ''
+    deadnix --fail ${flake}
+    #statix check ${flake} # https://github.com/nerdypepper/statix/issues/75
+    nixfmt --check ${flake}
+    touch $out
+  ''
